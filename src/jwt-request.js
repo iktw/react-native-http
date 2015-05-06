0e4cade98895c227401019a1f6ea0dd77534ccac
Mohammed Hammoud <mohammed@iktw.se>
2015-05-06 22:33:48 +0200

    Add default methods to manage requests

--- src/jwt-request.js
+++ src/jwt-request.js
@@ -2,15 +2,54 @@ var JWTHelper = require('./jwt-helper');
 var JWTConfig = require('./jwt-config');
 
 var JWTRequest = {
-    setAuthorizationHeader(options, token) {
-        if (!options.headers) options.headers = {};
-        options.headers[JWTConfig.authHeader] = `${JWTConfig.authPrefix} ${token}`;
+    status(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response
+        }
+        throw Error(null) // Todo: Return response error here
+    },
 
+    json(response) {
+        return response.json()
+    },
+
+    setDefaultHeaders(options) {
+        options.headers = options.headers || {};
+        if (!options.headers['Accept']) {
+            options.headers['Accept'] = 'application/json';
+        }
+
+        if (!options.headers['Content-Type']) {
+            options.headers['Content-Type'] = 'application/json';
+        }
+        return options;
+    },
+
+    setDefaultBody(options) {
+        options.body = options.body || {};
+        if (options.headers['Content-Type']) {
+            options.body = JSON.stringify(options.body);
+        }
+        return options;
+    },
+
+    setAuthorizationHeader(options, token) {
+        options.headers = options.headers || {};
+        options.headers[JWTConfig.authHeader] = `${JWTConfig.authPrefix} ${token}`;
         return options;
     },
 
     handleUnAuthorizedFetch(url, options) {
-        return fetch(url, options).then((response) => response.json());
+        return new Promise((resolve, reject) => {
+            fetch(url, options)
+                .then(this.status)
+                .then(this.json)
+                .then((json) => {
+                    resolve(json);
+                }).catch((error) => {
+                    reject(error);
+                });
+        });
     },
 
     handleAuthorizedFetch(url, options) {
@@ -18,9 +57,14 @@ var JWTRequest = {
             JWTHelper.getToken().then((token) => {
                 if (token && !JWTHelper.isTokenExpired(token)) {
                     options = this.setAuthorizationHeader(options, token);
-                    fetch(url, options).then((response) => {
-                        resolve(response.json())
-                    });
+                    fetch(url, options)
+                        .then(this.status)
+                        .then(this.json)
+                        .then(function (json) {
+                            resolve(json);
+                        }).catch(function (error) {
+                            reject(error);
+                        });
                 } else {
                     reject('Token is either not valid or has expired.');
                 }
@@ -30,6 +74,8 @@ var JWTRequest = {
 
     fetch(url, options) {
         options = options || {};
+        options = this.setDefaultHeaders(options);
+        options = this.setDefaultBody(options);
 
         if (options.skipAuthorization) {
             return this.handleUnAuthorizedFetch(url, options);
